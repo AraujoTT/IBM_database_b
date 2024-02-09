cdcdfe572aecc8fb06e01aee93d03fbb3f072222
AraujoTT <luisthiago642@gmail.com>
2024-02-08 21:30:26 -0300

    fazendo exercicio proposto e arendendo sobre utilizaçao de WHERE e DISTINCT

---
+++ aula/SQLite.sql
@@ -0,0 +1,5 @@
+-- SELECT DISTINCT Cliente FROM tabelapedidos;
+
+-- utilizando a funçao a baixo para localizar e db
+-- e filtrar por pais de origem na tabela fornecedores
+ SELECT * FROM tabelafornecedores WHERE País_de_Origem = 'China';
