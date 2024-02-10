86d18ca67cf2edc0cecf340093be21370ebac6ab
AraujoTT <luisthiago642@gmail.com>
2024-02-09 22:09:11 -0300

    criando tabela com criate table

--- aula/SQLite.sql
+++ aula/SQLite.sql
@@ -1,5 +1,15 @@
--- SELECT DISTINCT Cliente FROM tabelapedidos;
+CREATE TABLE tabelaclientes (
+    ID_Cliente INT PRIMARY KEY,
+    Nome_Cliente VARCHAR (250),
+    Informacoes_de_Contato VARCHAR (250)
+    );
+
+SELECT * FROM tabelaclientes;
+
+-- funçao criada para puxar a informaçoes de id sem repetiçao 
+--SELECT DISTINCT Cliente FROM tabelapedidos;
+
 
 -- utilizando a funçao a baixo para localizar e db
 -- e filtrar por pais de origem na tabela fornecedores
- SELECT * FROM tabelafornecedores WHERE País_de_Origem = 'China';
+-- SELECT * FROM tabelafornecedores WHERE País_de_Origem = 'China';
