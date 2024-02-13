3a0e6e0211c5daa48375f24bffe2bb7f15ca638c
AraujoTT <luisthiago642@gmail.com>
2024-02-12 22:49:56 -0300

    criando uma nova tabela e tambem inserindo 1 dados

---
+++ aula/SQLite3.sql
@@ -0,0 +1,9 @@
+INSERT INTO tabelaclientes(
+    ID_Cliente,
+    Nome_Cliente,
+    Informacoes_de_Contato,
+    endereco_cliente)
+    VALUES(
+        '1','Ana Silva', 'ana.silva@email.com', 'Rua flores- casa 1 '
+    );
+    SELECT * FROM tabelaclientes;
