9951f1bcea20eece96ac9641cbb85b8d9cbe637b
AraujoTT <luisthiago642@gmail.com>
2024-02-12 23:00:34 -0300

    inserindo mais clientes

---
+++ aula/SQLite4.sql
@@ -0,0 +1,11 @@
+INSERT INTO tabelaclientes(
+    ID_Cliente,
+    Nome_Cliente,
+    Informacoes_de_Contato,
+    endereco_cliente)
+    VALUES 
+    ('2', 'João Santos', 'joao.santos@provedor.com', 'rua dos pinheiros- 25'),
+    ('3', 'Maria Fernandes', 'maria.fernandes@email.com', 'Rua Santo Antonio, 10'),
+    ('4', 'Carlos Pereira', 'carlos.pereira@email.com', 'Avenida rio, 67');
+
+.mode
