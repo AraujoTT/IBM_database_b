53343834c32c21b75e479a7a8c2c8ca44e8e7d37
AraujoTT <luisthiago642@gmail.com>
2024-02-16 23:09:55 -0300

    .

--- aula/SQLite8.sql
+++ aula/SQLite8.sql
@@ -1,5 +1,5 @@
 --selecionar intervado de objetos 
-SELECT * FROM tabelapedidos WHERE Data_de_Envio_Estimada BETWEEN '2023-08-01' AND '2023-09-01';
+SELECT * FROM tabelapedidos WHERE Data_de_Envio_Estimada BETWEEN '2023-08-01' AND '2023-09-02';
 
 -- fazendo filtro com estatus de negaçao sobre objeto selecionado
 --SELECT * FROM tabelapedidos WHERE NOT status = 'Pendente';
